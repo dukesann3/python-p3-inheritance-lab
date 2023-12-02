6684cfe29dd7f2827d18f4c90c8e9e155c1763a0
Duke <dukemorise33@gmail.com>
2023-12-01 20:55:47 -0800

    completed lesson

--- lib/teacher.py
+++ lib/teacher.py
@@ -3,8 +3,26 @@
 from user import User
 
 import random
+import math
+
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+
     def teach(self):
-        pass
+        random_index = math.floor(random.randrange(0,len(knowledge)))
+        piece_of_knowledge = knowledge[random_index]
+        return piece_of_knowledge
